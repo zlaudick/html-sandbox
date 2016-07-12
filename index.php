b61baee29701af2ae7896cfbf0ce2cbee855c833
Zac Laudick <zlaudick@nmsu.edu>
2016-07-12 12:52:09 -0600

    added paragraphs and lists

--- index.php
+++ index.php
@@ -6,5 +6,21 @@
 	</head>
 	<body>
 		<h1>Hi World!</h1>
+		<p>Barkadeer Chain Shot chase matey parley aye reef belaying pin cackle fruit swab. Buccaneer smartly dance the hempen jig jack tender haul wind pinnace chandler hempen halter keel. Black jack Pirate Round mutiny matey handsomely Blimey ye crimp wench ho. Topsail fluke hail-shot dance the hempen jig driver schooner parrel belaying pin Plate Fleet sheet. Piracy pirate yo-ho-ho Blimey Pirate Round starboard overhaul case shot measured fer yer chains walk the plank.</p>
+		<h2>About Me</h2>
+		<p>Sheet nipper chase guns skysail grog blossom Sea Legs mizzen spirits sutler blow the man down. Yard code of conduct quarter bounty pressgang fore topmast line Barbary Coast parley. Deadlights belaying pin marooned Chain Shot take a caulk bring a spring upon her cable hempen halter Plate Fleet lookout Jack Tar.</p>
+		<ul>
+			<li>Item 1</li>
+			<li>Item 2</li>
+			<li>Item 3</li>
+		</ul>
+		<ol>
+			<li>Item 1</li>
+			<li>Item 2</li>
+			<li>Item 3</li>
+		</ol>
+		<div>
+			<a href="https://google.com" target="_blank">Go to Google.com</a>
+		</div>
 	</body>
 </html>
