05936f30859c085579c8ffc9aae2704b2ea60b9b
Zac Laudick <zlaudick@nmsu.edu>
2016-08-04 11:46:53 -0600

    added navbar

--- index.php
+++ index.php
@@ -37,16 +37,50 @@
 	</head>
 	<body class="sfooter">
 		<div class="sfooter-content">
+
 			<!-- begin header -->
 			<header>
 				<div class="container">
 					<div class="row">
 						<div class="col-md-12">
-							<h1>My Bootstrap Page</h1>
+
+							<!-- begin navbar -->
+							<nav class="navbar navbar-default">
+								<!-- Brand and toggle get grouped for better mobile display -->
+								<div class="navbar-header">
+									<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+										<span class="sr-only">Toggle navigation</span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+										<span class="icon-bar"></span>
+									</button>
+									<a class="navbar-brand" href="#">My Bootstrap Page</a>
+								</div>
+
+								<!-- Collect the nav links, forms, and other content for toggling -->
+								<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+									<ul class="nav navbar-nav navbar-right">
+										<li><a href="#">Link</a></li>
+										<li class="dropdown">
+											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Moar Links <span class="caret"></span></a>
+											<ul class="dropdown-menu">
+												<li><a href="#">Action</a></li>
+												<li><a href="#">Another action</a></li>
+												<li><a href="#">Something else here</a></li>
+												<li role="separator" class="divider"></li>
+												<li><a href="#">Separated link</a></li>
+											</ul>
+										</li>
+									</ul>
+								</div><!-- /.navbar-collapse -->
+
+							</nav>
 						</div>
 					</div>
 				</div>
 			</header>
+
+			<!-- begin main content -->
 			<main>
 				<div class="container">
 					<div class="row">
@@ -58,13 +92,13 @@
 						</div>
 					</div>
 					<div class="row">
-						<div class="col-md-4">
+						<div class="col-md-4 col-sm-6">
 							column 1
 						</div>
-						<div class="col-md-4">
+						<div class="col-md-4 col-sm-6">
 							column 2
 						</div>
-						<div class="col-md-4">
+						<div class="col-md-4 col-sm-6">
 							column 3
 						</div>
 					</div>
